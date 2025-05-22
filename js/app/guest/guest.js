2f3b48fe7718e1ae99ccaefb773d894e06ef44b3
xapty <septiantriutama@gmail.com>
2025-05-22 18:33:50 +0700

    changes 11

--- js/app/guest/guest.js
+++ js/app/guest/guest.js
@@ -245,10 +245,10 @@ export const guest = (() => {
         const url = new URL('https://calendar.google.com/calendar/render');
         const data = new URLSearchParams({
             action: 'TEMPLATE',
-            text: 'The Wedding of Wahyu and Riski',
-            dates: `${formatDate('2023-03-15 10:00')}/${formatDate('2023-03-15 11:00')}`,
+            text: 'Pernikahan Asel dan Aci',
+            dates: `${formatDate('2025-06-22 09:00')}/${formatDate('2025-06-22 10:00')}`,
             details: 'Tanpa mengurangi rasa hormat, kami mengundang Anda untuk berkenan menghadiri acara pernikahan kami. Terima kasih atas perhatian dan doa restu Anda, yang menjadi kebahagiaan serta kehormatan besar bagi kami.',
-            location: 'RT 10 RW 02, Desa Pajerukan, Kec. Kalibagor, Kab. Banyumas, Jawa Tengah 53191.',
+            location: 'RT 03 RW 16, Kp. Kertamanah, Kec. Pangalengan, Kab. Bandung, Jawa Barat 40378.',
             ctz: config.get('tz'),
         });
 
